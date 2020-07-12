71deed911c09390e046f69663fba3ef7c869c897
Joshua Baxter <jbreezy248@gmail.com>
2020-07-12 14:56:06 -0400

    Made the following modifications: 1. Using a bootstrap carousel to support swiping the cards to get to the next one. 2. Cards now take up the whole screen. Can swipe to the left or the right to view the next one or previous one.

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -58,58 +58,75 @@ $( document ).ready(function() {
     
     
 
-    var card1 = document.getElementById('card1');
-    
-    card1.addEventListener( 'click', function() {
-        var cardFront = card1.querySelector('.card-front');
-        cardFront.classList.toggle('is-flipped');
-        var cardBack = card1.querySelector('.card-back');
-        cardBack.classList.toggle('is-flipped');
-        
-        });
+ //Logic for the carousel: setting each carousel
+ //item to take up the entire screen
+ var $item = $('.carousel-item'); 
+ var $wHeight = $(window).height();
+ $item.eq(0).addClass('active');
+ $item.height($wHeight); 
+ $item.addClass('full-screen');
 
-    
+ 
+ $(window).on('resize', function (){
+   $wHeight = $(window).height();
+   $item.height($wHeight);
+ });
+ 
+//  $('.carousel').carousel({
+//    interval: false
+//  });
 
-    var card2 = document.getElementById('card2');
-    
-    card2.addEventListener( 'click', function() {
-        var cardFront = card2.querySelector('.card-front');
-        cardFront.classList.toggle('is-flipped');
-        var cardBack = card2.querySelector('.card-back');
-        cardBack.classList.toggle('is-flipped');
-        
-        });
+ //Setting up the cards
+ var card1 = document.getElementById('card1');
+ 
+ //Add listener so that the card will flip when it is clicked on
+ card1.addEventListener( 'click', function() {
+     var cardFront = card1.querySelector('.card-front');
+     cardFront.classList.toggle('is-flipped');
+     var cardBack = card1.querySelector('.card-back');
+     cardBack.classList.toggle('is-flipped');
+     
+     });
 
-    var card3 = document.getElementById('card3');
-    
-    card3.addEventListener( 'click', function() {
-        var cardFront = card3.querySelector('.card-front');
-        cardFront.classList.toggle('is-flipped');
-        var cardBack = card3.querySelector('.card-back');
-        cardBack.classList.toggle('is-flipped');
-        
-        });
+ var card2 = document.getElementById('card2');
+ 
+ card2.addEventListener( 'click', function() {
+     var cardFront = card2.querySelector('.card-front');
+     cardFront.classList.toggle('is-flipped');
+     var cardBack = card2.querySelector('.card-back');
+     cardBack.classList.toggle('is-flipped');
+     
+     });
 
-    var card4 = document.getElementById('card4');
+ var card3 = document.getElementById('card3');
+ 
+ card3.addEventListener( 'click', function() {
+     var cardFront = card3.querySelector('.card-front');
+     cardFront.classList.toggle('is-flipped');
+     var cardBack = card3.querySelector('.card-back');
+     cardBack.classList.toggle('is-flipped');
+     
+     });
 
-    card4.addEventListener( 'click', function() {
-        var cardFront = card4.querySelector('.card-front');
-        cardFront.classList.toggle('is-flipped');
-        var cardBack = card4.querySelector('.card-back');
-        cardBack.classList.toggle('is-flipped');
-        
-        });
+ var card4 = document.getElementById('card4');
 
-    var card5 = document.getElementById('card5');
+ card4.addEventListener( 'click', function() {
+     var cardFront = card4.querySelector('.card-front');
+     cardFront.classList.toggle('is-flipped');
+     var cardBack = card4.querySelector('.card-back');
+     cardBack.classList.toggle('is-flipped');
+     
+     });
 
-    card5.addEventListener( 'click', function() {
-        var cardFront = card5.querySelector('.card-front');
-        cardFront.classList.toggle('is-flipped');
-        var cardBack = card5.querySelector('.card-back');
-        cardBack.classList.toggle('is-flipped');
-        
-        });
+ var card5 = document.getElementById('card5');
 
+ card5.addEventListener( 'click', function() {
+     var cardFront = card5.querySelector('.card-front');
+     cardFront.classList.toggle('is-flipped');
+     var cardBack = card5.querySelector('.card-back');
+     cardBack.classList.toggle('is-flipped');
+     
+     });
     
     
     
--- www/js/index.js
+++ www/js/index.js
@@ -58,58 +58,73 @@ $( document ).ready(function() {
     
     
 
-    var card1 = document.getElementById('card1');
-    
-    card1.addEventListener( 'click', function() {
-        var cardFront = card1.querySelector('.card-front');
-        cardFront.classList.toggle('is-flipped');
-        var cardBack = card1.querySelector('.card-back');
-        cardBack.classList.toggle('is-flipped');
-        
-        });
+ //Logic for the carousel: setting each carousel
+ //item to take up the entire screen
+ var $item = $('.carousel-item'); 
+ var $wHeight = $(window).height();
+ $item.eq(0).addClass('active');
+ $item.height($wHeight); 
+ $item.addClass('full-screen');
 
-    
+ //Set each carousel item to take up
+ //screen if the orientation of the screen changes
+ //(for example, someone rotating the phone to make it horziontal instead of vertical)
+ $(window).on('resize', function (){
+   $wHeight = $(window).height();
+   $item.height($wHeight);
+ });
 
-    var card2 = document.getElementById('card2');
-    
-    card2.addEventListener( 'click', function() {
-        var cardFront = card2.querySelector('.card-front');
-        cardFront.classList.toggle('is-flipped');
-        var cardBack = card2.querySelector('.card-back');
-        cardBack.classList.toggle('is-flipped');
-        
-        });
+ //Setting up the cards
+ var card1 = document.getElementById('card1');
+ 
+ //Add listener so that the card will flip when it is clicked on
+ card1.addEventListener( 'click', function() {
+     var cardFront = card1.querySelector('.card-front');
+     cardFront.classList.toggle('is-flipped');
+     var cardBack = card1.querySelector('.card-back');
+     cardBack.classList.toggle('is-flipped');
+     
+     });
 
-    var card3 = document.getElementById('card3');
-    
-    card3.addEventListener( 'click', function() {
-        var cardFront = card3.querySelector('.card-front');
-        cardFront.classList.toggle('is-flipped');
-        var cardBack = card3.querySelector('.card-back');
-        cardBack.classList.toggle('is-flipped');
-        
-        });
+ var card2 = document.getElementById('card2');
+ 
+ card2.addEventListener( 'click', function() {
+     var cardFront = card2.querySelector('.card-front');
+     cardFront.classList.toggle('is-flipped');
+     var cardBack = card2.querySelector('.card-back');
+     cardBack.classList.toggle('is-flipped');
+     
+     });
 
-    var card4 = document.getElementById('card4');
+ var card3 = document.getElementById('card3');
+ 
+ card3.addEventListener( 'click', function() {
+     var cardFront = card3.querySelector('.card-front');
+     cardFront.classList.toggle('is-flipped');
+     var cardBack = card3.querySelector('.card-back');
+     cardBack.classList.toggle('is-flipped');
+     
+     });
 
-    card4.addEventListener( 'click', function() {
-        var cardFront = card4.querySelector('.card-front');
-        cardFront.classList.toggle('is-flipped');
-        var cardBack = card4.querySelector('.card-back');
-        cardBack.classList.toggle('is-flipped');
-        
-        });
+ var card4 = document.getElementById('card4');
 
-    var card5 = document.getElementById('card5');
+ card4.addEventListener( 'click', function() {
+     var cardFront = card4.querySelector('.card-front');
+     cardFront.classList.toggle('is-flipped');
+     var cardBack = card4.querySelector('.card-back');
+     cardBack.classList.toggle('is-flipped');
+     
+     });
 
-    card5.addEventListener( 'click', function() {
-        var cardFront = card5.querySelector('.card-front');
-        cardFront.classList.toggle('is-flipped');
-        var cardBack = card5.querySelector('.card-back');
-        cardBack.classList.toggle('is-flipped');
-        
-        });
+ var card5 = document.getElementById('card5');
 
+ card5.addEventListener( 'click', function() {
+     var cardFront = card5.querySelector('.card-front');
+     cardFront.classList.toggle('is-flipped');
+     var cardBack = card5.querySelector('.card-back');
+     cardBack.classList.toggle('is-flipped');
+     
+     });
     
     
     
